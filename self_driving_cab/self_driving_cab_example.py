30a6939808583f33bab0bf8406e6702db3941cd6
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-11-20 17:11:29 +0000

    Added Q-Learning agent

--- self_driving_cab/self_driving_cab_example.py
+++ self_driving_cab/self_driving_cab_example.py
@@ -1,5 +1,7 @@
 import gym
 import os
+import numpy as np
+import random
 from time import sleep
 
 def print_frames(frames):
@@ -60,4 +62,80 @@ value = input("Printing frames (will clear screen) (Y/n)")
 
 if value == 'Y':
   print_frames(frames)
+
+# Solving with RL (Q-Learning)
+print()
+print('Solving w/ Rl (Q-Learning)')
+print('-'*20)
+
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+
+# Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+# for plotting metrics
+all_epochs = []
+all_penalties = []
+
+for i in range(1, 100001):
+  state = env.reset()
+  epochs, penalties, reward = 0, 0, 0
+  done = False
+
+  while not done:
+    if random.uniform(0, 1) < epsilon:
+      action = env.action_space.sample() # explore action space
+    else:
+      action = np.argmax(q_table[state]) # exploit learned values
+
+    next_state, reward, done, info = env.step(action)
+
+    old_value = q_table[state, action]
+    next_max = np.max(q_table[next_state])
+
+    new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+    q_table[state, action] = new_value
+
+    if reward == -10:
+      penalties += 1
+
+    state = next_state
+    epochs += 1
+
+  if i % 100 == 0:
+    os.system('clear')
+    print(f"Episode: {i}")
+
+print("Training finished.\n")
+
+# Evaluate agents performance after Q-Learning
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+  state = env.reset()
+  epochs, penalties, reward = 0, 0, 0
+  done = False
+
+  while not done:
+    action = np.argmax(q_table[state])
+    state, reward, done, info = env.step(action)
+
+    if reward == -10:
+      penalties += 1
+
+    epochs += 1
+
+  total_penalties += penalties
+  total_epochs += epochs
+
+print(f"Results after {episodes} episodes")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}")
+
+
+
+
 
